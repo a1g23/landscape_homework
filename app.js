778b85a515e3bcbe3a1220533ba5f600f7aeb896
Alex Greenberg <acgreenberg2@gmail.com>
2023-11-10 16:56:36 -0500

    teeth and scissors

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("landscaper")
+
+// first lets give the option with a prompt, for the player to log how many lawns they cut today
+const lawnsCutTeeth = prompt("Working today? How many lawns did you cut with your teeth?", 0)
+
+// if the player cut more than one lawn with their teeth, they made $1 multiplied by how many lawns they cut
+if(lawnsCutTeeth > 0 && lawnsCutTeeth < 5) {
+    alert("we made $" + (lawnsCutTeeth * 1) + " today")
+} else if (lawnsCutTeeth >= 5) {
+    var scissors = prompt("we made $" + (lawnsCutTeeth * 1) + " today. Do want to up your game with some scissors?", "yes") 
+}
+
+if(scissors === "yes") {
+    alert("great investment, you'll make $5 per lawn now!")
+}
+
+
+
+
+
+
+
+
+
+// the player starts at 0 lawns
+
