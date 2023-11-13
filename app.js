a6fdac43fd2728a98e28dc03659f712a0902a54c
Alex Greenberg <acgreenberg2@gmail.com>
2023-11-12 19:22:19 -0500

    couple changes

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-console.log("landscaper")
-
 // adding a player object to track money, the method they currently have and if they won the game
-
 const player = {
     money: 0,
     methodArr: 0,
@@ -18,7 +15,8 @@ const method = [
     {name: "Starving Students", cost: 500, revenue: 250}
 ]
 
-
+// function to win the game
+// player has to have more than 1000 and the method array they are using needs to be greater than the length of the array (ie the last method)
 const gameWon = () => {
     if (player.money >= 1000 && player.methodArr >= method.length) {
         alert("You won the game!!!")
@@ -27,41 +25,47 @@ const gameWon = () => {
     }
 }
 
-const cutLawn = () => {
-    
+gameWon()
+
+// function to cut lawn, we want a prompt to ask how many lawns cut and then alert them with progress
+function cutLawn(){
+    prompt("Working today? How many lawns did you cut with", 0)
 }
+
+cutLawn();
+
 
 // first lets give the option with a prompt, for the player to log how many lawns they cut today
-const lawnsCutTeeth = prompt("Working today? How many lawns did you cut with your teeth?", 0)
+// const lawnsCutTeeth = prompt("Working today? How many lawns did you cut with your teeth?", 0)
 
 
-// if the player cut more than one lawn with their teeth, they made $1 multiplied by how many lawns they cut. If they
-// make more than $5 dollars, they can buy scissors and make $5 per lawn
-if(lawnsCutTeeth > 0 && lawnsCutTeeth < 5) {
-    alert("we made $" + (lawnsCutTeeth * 1) + " today")
-} else if (lawnsCutTeeth >= 5) {
-    const scissors = prompt("we made $" + (lawnsCutTeeth * 1) + " today. Do want to up your game with some scissors?", "yes") 
-    if(scissors === "yes") {
-        alert("great investment, you'll make $5 per lawn now!")
-    }
-}
+// // if the player cut more than one lawn with their teeth, they made $1 multiplied by how many lawns they cut. If they
+// // make more than $5 dollars, they can buy scissors and make $5 per lawn
+// if(lawnsCutTeeth > 0 && lawnsCutTeeth < 5) {
+//     alert("we made $" + (lawnsCutTeeth * 1) + " today")
+// } else if (lawnsCutTeeth >= 5) {
+//     const scissors = prompt("we made $" + (lawnsCutTeeth * 1) + " today. Do want to up your game with some scissors?", "yes") 
+//     if(scissors === "yes") {
+//         alert("great investment, you'll make $5 per lawn now!")
+//     }
+// }
 
-// if the player bought scissors, we can ask them what the
-const teethOrScissors = prompt("Back at it! Did you use teeth or scissors today?","scissors")
+// // if the player bought scissors, we can ask them what the
+// const teethOrScissors = prompt("Back at it! Did you use teeth or scissors today?","scissors")
 
-if(teethOrScissors === "scissors") {
-    const lawnsCutScissors = prompt("Good choice, how many lawns did you cut?",0)
-    if(lawnsCutScissors > 0 && lawnsCutScissors < 5) {
-        alert("we made $" + (lawnsCutScissors * 5) + " today")
-    } else if (lawnsCutScissors >= 5) {
-        const lawnmower = prompt("we made $" + (lawnsCutScissors * 5) + " today. Do want to up your game with a old lawnmower?", "yes") 
-        if(lawnmower === "yes") {
-            alert("great investment, you'll make $50 per lawn now!")
-        }
-    }
-} else {
-    alert("come on! you won't make it in this business unless you invest in better equipment!")
-}
+// if(teethOrScissors === "scissors") {
+//     const lawnsCutScissors = prompt("Good choice, how many lawns did you cut?",0)
+//     if(lawnsCutScissors > 0 && lawnsCutScissors < 5) {
+//         alert("we made $" + (lawnsCutScissors * 5) + " today")
+//     } else if (lawnsCutScissors >= 5) {
+//         const lawnmower = prompt("we made $" + (lawnsCutScissors * 5) + " today. Do want to up your game with a old lawnmower?", "yes") 
+//         if(lawnmower === "yes") {
+//             alert("great investment, you'll make $50 per lawn now!")
+//         }
+//     }
+// } else {
+//     alert("come on! you won't make it in this business unless you invest in better equipment!")
+// }
 
 
 
